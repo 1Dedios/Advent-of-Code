dc2a7eba944219792e9375c3277216a3f87946ec
Dedios <dediosprogramming@gmail.com>
2025-01-18 22:44:25 -0500

    Fix: input file change and refactor

--- 2024/distanceBetween.js
+++ 2024/distanceBetween.js
@@ -1,8 +1,8 @@
 import { readFile } from 'fs';
 
-readFile('./input1.txt', 'utf8', (error, text) => {
+readFile('./input.txt', 'utf8', (error, text) => {
   if (error) throw error;
-  const firstLine = text.split('/n');
+  const firstLine = text.split('\n');
   // replace(pattern, replacement)
   console.log(firstLine);
 });
