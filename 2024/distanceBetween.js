90af31933f7fad6564e207c1cb34ee5855e1a6e6
Dedios <dediosprogramming@gmail.com>
2025-01-18 22:47:25 -0500

    Refactor: Thrown Error

--- 2024/distanceBetween.js
+++ 2024/distanceBetween.js
@@ -1,7 +1,7 @@
 import { readFile } from 'fs';
 
 readFile('./input.txt', 'utf8', (error, text) => {
-  if (error) throw error;
+  if (error) throw Error(error);
   const firstLine = text.split('\n');
   // replace(pattern, replacement)
   console.log(firstLine);
