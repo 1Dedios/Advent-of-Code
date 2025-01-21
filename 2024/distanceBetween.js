0eb4bc09bd8040dd174f8e60e05f836bf96debb3
Dedios <dediosprogramming@gmail.com>
2025-01-21 16:41:24 -0500

    Fix: cleaned up and segmented data

--- 2024/distanceBetween.js
+++ 2024/distanceBetween.js
@@ -1,8 +1,17 @@
-import { readFile } from 'fs';
+import { open } from 'node:fs/promises';
 
-readFile('./input.txt', 'utf8', (error, text) => {
-  if (error) throw Error(error);
-  const firstLine = text.split('\n');
-  // replace(pattern, replacement)
-  console.log(firstLine);
-});
+const file = await open('./input.txt');
+
+let colOneArray = [];
+let colTwoArray = [];
+
+for await (const line of file.readLines()) {
+  let lineArray = line.split('  ')
+  colOneArray.push(parseInt(lineArray[0]))
+  colTwoArray.push(parseInt(lineArray[1]))
+}
+console.log(colOneArray.length === colTwoArray.length)
+
+// each array has their corresponding numbers
+// remove the min num from each array and subtract - convert difference to absolute value and aggregate in one array
+// aggregated array should be summed and that should be the answer for the first day's challenge
