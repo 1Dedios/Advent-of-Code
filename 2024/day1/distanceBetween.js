d50f77cb74836eeb33b10ed548e9a95648375591
Dedios <dediosprogramming@gmail.com>
2025-01-21 19:05:10 -0500

    outline for how to solve day1 pt2

--- 2024/day1/distanceBetween.js
+++ 2024/day1/distanceBetween.js
@@ -32,3 +32,23 @@ while (lenOfArrays > 0) {
 }
 
 console.log(aggregateArray.reduce((acc, val) => acc + val, 0))
+
+/** 
+ * 
+ * PART 2
+ * 
+ */
+
+// need to create key, val pairs - key=colOneArrayCopy elements, val=how often it appears in colTwoArrayCopy
+
+// iterate colOneArrayCopy and make element the key and 0 as val
+
+// iterate colTwoArrayCopy - if element a key in the object - if val > 0 increase val by one else change val to 1
+
+// create arrays from keys and values
+
+// create similarity array - empty 
+
+// iterate through keys - take keys and multiply by val array element and push resulting value to similarity array
+
+// reduce function on similarity array should be the answer
