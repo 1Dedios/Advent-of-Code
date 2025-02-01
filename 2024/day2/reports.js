1e58cf11baf76193034e3805d60dc2546453e194
Dedios <dediosprogramming@gmail.com>
2025-01-31 22:56:57 -0500

    outlining the approach

--- 2024/day2/reports.js
+++ 2024/day2/reports.js
@@ -11,34 +11,42 @@ for await (const report of logsFile.readLines()) {
 }
 
 for (const report of reportAggregator) {
-  // iterate through reports - each report should be split by space
-  // push the clean reports to clean report array as an array
   cleanReports.push(report.split(' '));
 }
+
 // clean report array should now be a 2D array
 console.log(cleanReports);
 
 /**
  *
- * SAFE REPORT
+ * WHAT IS A SAFE REPORT?
  *
  * - all levels are increasing or decreasing
  * - any 2 adjacent levels differ by at least one or at most three
  */
 // extract the number of reports that are considered "safe" and return that number that's the answer to part 1
 // each col of a report represents a level
-// iterate through clean reports levels w/ a fixed window size of 2 from the current element if it qualifies for safe report increase safeReport #
+// iterate through clean reports levels w/ a fixed window size of 2 from the current element if it qualifies for safe report increase safeReport VAR #
 
 for (const reportCell of cleanReports) {
-  // going through the report
+  // going through each report and below iterating through each level/element
   for (let i = 0; i < reportCell.length; i++) {
-    // compare the first cell to the next cell if there is a difference bigger than 3 then continue
+    // compare the first level to the next level if there is a difference bigger than 3 then continue - DISQUALIFIED
     if (
       Math.abs(parseInt(reportCell[i], 10) - parseInt(reportCell[i++], 10)) > 3
     ) {
       continue;
     } else {
-      // now we can start going through the report with a fixed window size of 2
+      let increasing = false;
+      let decreasing = false;
+
+      reportCell[i] > reportCell[i++]
+        ? (decreasing = true)
+        : (increasing = true);
+
+      // depending on the flag set then the report levels ALL should be following the flag if it deviates then report UNSAFE (move to next)
+      // now we can start going through the levels with a fixed window size of 2
+      // if the levels in this new window don't differ by a difference > 3 - and also adhere to the flag - still safe
     }
   }
 }
